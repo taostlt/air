c77c6caf08098f67c6379313df2f701607aff25c
taostlt@gmail.com <taostlt@gmail.com>
2019-09-18 06:32:33 -0700

    Save before experimenting with adding game loop time to predicted path.

--- sample_clock.py
+++ sample_clock.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 pygame.init()
 screen = pygame.display.set_mode((128, 128))
 clock = pygame.time.Clock()
@@ -8,14 +9,21 @@ pygame.time.set_timer(pygame.USEREVENT, 1000)
 font = pygame.font.SysFont('Consolas', 30)
 
 while True:
-    for e in pygame.event.get():
-        if e.type == pygame.USEREVENT:
+    for event in pygame.event.get():
+        if event.type == pygame.USEREVENT:
             counter -= 1
             text = str(counter).rjust(3) if counter > 0 else 'boom!'
-        if e.type == pygame.QUIT: break
+
+        if event.type == pygame.QUIT: break
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            print('ESCAPE KEY')
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            sys.exit(0)
     else:
-        screen.fill((255, 255, 255))
-        screen.blit(font.render(text, True, (0, 0, 0)), (37, 48))
+        screen.fill((25, 25, 25))
+        screen.blit(font.render(text, True, (255, 255, 255)), (37, 48))
         pygame.display.flip()
         clock.tick(60)
         continue
